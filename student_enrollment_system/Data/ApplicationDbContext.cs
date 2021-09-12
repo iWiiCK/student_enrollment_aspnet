ab6e40ce3a544f08c246178a618eaad10415d82b
Heshan <heshanwick2000@gmail.com>
2021-09-12 12:26:51 +0530

    feat: initial-commit

---
+++ student_enrollment_system/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using student_enrollment_system.Models;
+
+namespace student_enrollment_system.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<student_enrollment_system.Models.Student> Student { get; set; }
+    }
+}
---
+++ student_enrollment_system/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace student_enrollment_system.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string StudentName { get; set; }
+
+        public string ContactNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public Student()
+        { 
+
+        }
+    }   
+}
+        
